4410c134163e351f14298b70b8a011a24f3c2885
lmc-cmd <63010584+lmc-cmd@users.noreply.github.com>
2022-06-30 20:28:03 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from 'simplelightbox';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const makeGalleryArr = galleryItems.map(e => {
+  const galarryItemEl = `
+  <a class="gallery__item" href=${e.original}>
+  <img class="gallery__image" src=${e.preview} alt=${e.description} />
+</a>`;
+  return galarryItemEl;
+});
+
+const makeGallery = makeGalleryArr.join('');
+galleryEl.insertAdjacentHTML('afterbegin', makeGallery);
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionPosition: 'bottom',
+  captionsData: 'alt',
+  navtext: ['<~', '~>'],
+});

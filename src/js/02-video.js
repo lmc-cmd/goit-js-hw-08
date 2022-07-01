c7026f9d5d3a30f53518f260def2e15ea9a9f236
lmc-cmd <63010584+lmc-cmd@users.noreply.github.com>
2022-07-01 15:43:41 +0300

    (

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,44 @@
+'use strict';
+// import Player from '../@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+
+// const idPlayer = new Vimeo.Player('player1');
+// const player = new Player('handstick', {
+//   id: 19231868,
+//   width: 640,
+// });np
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// + [getCurrentTime](#getcurrenttime-promisenumber-error)
+//     + [setCurrentTime](#setcurrenttimeseconds-number-promisenumber-rangeerrorerror)
+//     + [getDuration](#getduration-promisenumber-error)
+
+// player
+//   .getCurrentTime()
+//   .then(function (seconds) {
+//     // seconds = the current playback position
+//   })
+//   .catch(function (error) {
+//     // an error occurred
+//   });
+
+// player
+//   .setCurrentTime(30.456)
+//   .then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });

b44b98c89acbcd01d03369e21a06a3350c22e949
lmc-cmd <63010584+lmc-cmd@users.noreply.github.com>
2022-07-06 14:38:51 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -24,8 +24,6 @@ function onSubmitForm(e) {
   const data = JSON.parse(localStorage.getItem('feedback-form-state'));
   const email = document.querySelector('.feedback-form input');
   const message = document.querySelector('.feedback-form textarea');
-  if (data) {
-    email.value = data.email;
-    message.value = data.message;
-  }
+  data.email ? (email.value = data.email) : (email.value = ``);
+  data.message ? (message.value = data.message) : (message.value = ``);
 })();
